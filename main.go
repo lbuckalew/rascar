e4e44445f6f053f9c47021d67ecc3b4744bb99a8
Louis Buckalew <louisbuckalew@Louis-MacBook-Pro.local>
2016-11-11 21:49:10 -0600

    up

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import "github.com/lbuckalew/blulang"
+import "fmt"
 
 func main() {
+
+	fmt.Println("t1")
 	a := &blulang.Adapter{}
+	fmt.Println("t2")
 	if err := a.Init(); err != nil {
 		panic(err)
 	}
+	fmt.Println("t3")
 }
